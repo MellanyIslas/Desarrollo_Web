e7cda6cd6c9f24edc231d083ec58fd142e459213
MellanyIslas <mellanynislas@gmail.com>
2024-09-06 23:05:27 -0600

    Primer video

---
+++ workspace/springboot-service-celulares/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO celulares (name, marca, created_id) VALUES ('Samsung', 'S21', NOW());
+INSERT INTO celulares (name, marca, created_id) VALUES ('Samsung', 'S22', NOW());
+INSERT INTO celulares (name, marca, created_id) VALUES ('Samsung', 'S23', NOW());
+INSERT INTO celulares (name, marca, created_id) VALUES ('Apple', 'Iphone 14', NOW());
+INSERT INTO celulares (name, marca, created_id) VALUES ('Apple', 'Iphone 15', NOW());
+INSERT INTO celulares (name, marca, created_id) VALUES ('Xiaomi', 'Mi', NOW());
